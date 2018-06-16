28b6b7e7c023c6031d904caa295fd81751a88461
zyuiop <zyuiop@zyuiop.net>
2018-06-16 04:03:24 +0200

    Permissions inheritance

--- app/data/AuthenticatedUser.scala
+++ app/data/AuthenticatedUser.scala
@@ -6,7 +6,16 @@ import play.api.libs.json.Json
   * @author zyuiop
   */
 case class AuthenticatedUser(id: Int, lastname: String, firstname: String, email: String, permissions: Seq[String]) {
-  def hasPerm(perm: String) = permissions.contains(perm)
+  def hasPerm(perm: String) = {
+    if (permissions.contains(perm)) true
+    else {
+      // Check other permissions (i.e. admin.* for admin.sth)
+      val list = perm.split("\\.").toList
+
+      if (list.size < 2) false
+      else list.map(_ + ".").scanLeft("")(_ + _) take list.size map (_ + "*") exists permissions.contains
+    }
+  }
 }
 
 object AuthenticatedUser {
