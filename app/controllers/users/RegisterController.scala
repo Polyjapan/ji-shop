2023a8eb01b7bba42f9283d2ecdf3d26b872cade
zyuiop <zyuiop@zyuiop.net>
2019-04-24 12:47:15 +0200

    Handle new registration procedure

--- app/controllers/users/RegisterController.scala
+++ app/controllers/users/RegisterController.scala
@@ -1,55 +1,57 @@
 package controllers.users
 
+import ch.japanimpact.auth.api.AuthApi.AppTicketResponse
+import ch.japanimpact.auth.api.constants.GeneralErrorCodes
+import ch.japanimpact.auth.api.{AuthApi, TicketType}
+import constants.results.Errors._
 import javax.inject.Inject
 import models.ClientsModel
 import play.api.Configuration
 import play.api.data.Forms._
 import play.api.data._
 import play.api.i18n.I18nSupport
+import play.api.libs.json.{JsArray, Json}
 import play.api.libs.mailer._
 import play.api.mvc._
 import utils.HashHelper
+import utils.Implicits._
 
 import scala.concurrent.ExecutionContext
 
 /**
   * @author zyuiop
   */
-class RegisterController @Inject()(cc: MessagesControllerComponents, clients: ClientsModel, hash: HashHelper)(implicit ec: ExecutionContext, mailerClient: MailerClient, config: Configuration) extends MessagesAbstractController(cc) with I18nSupport {
-  private val registerForm = Form(mapping("email" -> email, "password" -> nonEmptyText(8), "lastname" -> nonEmptyText, "firstname" -> nonEmptyText, "captcha" -> nonEmptyText, "acceptNews" -> default(boolean, false))(RegisterForm.apply)(RegisterForm.unapply))
+class RegisterController @Inject()(cc: MessagesControllerComponents, clients: ClientsModel, hash: HashHelper, api: AuthApi)(implicit ec: ExecutionContext, mailerClient: MailerClient, config: Configuration) extends MessagesAbstractController(cc) with I18nSupport {
+  private val registerForm = Form(mapping("ticket" -> nonEmptyText, "lastname" -> nonEmptyText, "firstname" -> nonEmptyText, "acceptNews" -> default(boolean, false))(RegisterForm.apply)(RegisterForm.unapply))
 
-  case class RegisterForm(email: String, password: String, lastName: String, firstName: String, captcha: String, acceptNews: Boolean)
+  case class RegisterForm(ticket: String, lastName: String, firstName: String, acceptNews: Boolean)
 
   def postSignup = Action.async(parse.json) { implicit request => {
-    /*val form = registerForm.bindFromRequest
+    val form = registerForm.bindFromRequest
 
     form.fold( // We bind the request to the form
       formError(_).asFuture, formData => {
-        // Check the captcha
-        captchaService.checkCaptchaWithExpiration(formData.captcha).flatMap(result =>
-          if (!result.success) BadRequest.asError(ErrorCodes.CAPTCHA).asFuture
-
-          // If we have no error in the form itself we try to find the user data
-          else clients.findClient(formData.email).map { opt =>
-            if (opt.isDefined) {
-              // User already exists: don't create it
-              EmailVerifyEmail.sendAccountExistsEmail(formData.email)
-            } else {
-              val hashed = hash.hash(formData.password)
-              val emailCode = RandomUtils.randomString(30)
-
-              clients.createClient(data.Client(Option.empty, formData.lastName, formData.firstName, formData.email, Some(emailCode), hashed._2, hashed._1, acceptNewsletter = formData.acceptNews))
-
-
-              EmailVerifyEmail.sendVerifyEmail(formData.email, emailCode)
-            }
-
-            Ok(Json.obj("success" -> true, "errors" -> JsArray()))
+        if (!api.isValidTicket(formData.ticket)) {
+          notFound("ticket").asFuture
+        } else {
+          api.getAppTicket(formData.ticket).flatMap {
+            case Left(AppTicketResponse(userId, userEmail, TicketType.RegisterTicket)) =>
+              clients
+                .createClient(data.Client(Option.empty, userId, formData.lastName, formData.firstName, userEmail, acceptNewsletter = formData.acceptNews))
+                .map(r => Ok(Json.obj("success" -> true, "errors" -> JsArray())))
+            case Left(AppTicketResponse(_, _, TicketType.DoubleRegisterTicket)) =>
+              Ok(Json.obj("success" -> true, "errors" -> JsArray())).asFuture
+            case Left(AppTicketResponse(_, _, _)) =>
+              // We might want to handle Login tickets
+              notFound("ticket").asFuture
+            case Right(error) if error == GeneralErrorCodes.InvalidAppSecret =>
+              InternalServerError.asFuture
+            case Right(_) =>
+              notFound("ticket").asFuture
           }
-        )
+        }
       }
-    )*/
-    ???
+    )
   }
   }
 }
